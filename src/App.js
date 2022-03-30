a12651804cceec193089eb45cce39917a3ded24a
William Lucas <williamsantos1506@gmail.com>
2022-03-29 23:23:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Modal from "react-modal";
+import "./App.css";
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement("#root");
 
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Container">
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        overlayClassName="modal-overlay"
+        className="modal-content"
+      >
+        <h2>Hello - I am a modal!</h2>
+        <hr />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We maintain that accessibility is a key component of any modern web
+          application. As such, we have created this modal in such a way that it
+          fulfills the accessibility requirements of the modern web. We seek to
+          keep the focus on accessibility while providing a functional, capable
+          modal component for general use.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={closeModal}>Close</button>
+      </Modal>
     </div>
   );
 }
